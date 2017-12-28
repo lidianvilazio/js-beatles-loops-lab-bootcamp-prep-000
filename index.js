79194895ec9aac59bcf0e6fa64719622792a61da
Lidian Vilazio <lidianvilazio@hotmail.com>
2017-12-28 20:02:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function theBeatlesPlay(musicians, instruments) {
 
 
 function johnLennonFacts(array) {
-  var i = array.length
+  var i = array.length -1
   while(i >= 0) {
     array[i]+="!!!"
     i--
